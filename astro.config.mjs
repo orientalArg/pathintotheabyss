8acf2d7a2f156ba3de92d518c9ec06461804de57
orientalArg <orientalArg@outlook.com>
2026-05-10 17:01:05 -0300

    feat added local font support for karantina

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,13 @@
-import { defineConfig } from 'astro/config';
-
-// https://astro.build/config
-export default defineConfig({});
+import { defineConfig, fontProviders } from 'astro/config';
+export default defineConfig({
+  // This MUST be inside experimental
+  fonts: [
+    {
+      provider: fontProviders.google(),
+      name: "Karantina", // Google Fonts names are usually capitalized
+      cssVariable: "--karantina",
+      weights: ['300', '400', '500', '700'],
+      styles: ['normal'],
+    }
+  ],
+});
